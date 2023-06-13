753c2d2fac9fa105816870567af7a4999ae3d502
6equal0 <6equal0@gmail.com>
2023-06-13 09:49:24 +0900

    낵

---
+++ 6equal0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _6equal0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //강민기민강
+        }
+    }
+}
