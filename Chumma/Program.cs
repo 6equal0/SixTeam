13c67f0d13ad37b3b70136e88efe40ff6b8ce057
chunma1126 <azqsxazwdc@naver.com>
2023-06-16 00:28:40 +0900

    hopeHoise

--- Chumma/Program.cs
+++ Chumma/Program.cs
@@ -4,38 +4,45 @@ namespace Chumma
 {
     class Program
     {
+        static int checkPlayer;
+
         public static void Main(string[] args)
         {
-
             Hope hope = new Hope();
             Console.WriteLine("캐릭터를 선택하세요!\n");
             hope.Welcome();
             Console.WriteLine("\n캐릭터의 번호를 입력해주세요!");
 
-
             for (int i = 0; i <= 1;)
             {
                 int char_num = int.Parse(Console.ReadLine());
-                if (char_num != hope.player_number[i])
-                {
-                    Console.WriteLine("똑바로 입력하세요");
-                }
 
+                if (!hope.player_number.Contains(char_num))
+                {
+                    Console.WriteLine("그는 아직 술집에 오지 않았습니다");
+                }
                 else
                 {
-                    if (char_num >= 22)
+                    if (char_num == checkPlayer)
                     {
-
-                        Console.WriteLine("똑바로 입력하세요");
+                        Console.WriteLine("이미 선택한 캐릭터 입니다!");
                     }
                     else
                     {
-                        Console.WriteLine($"{hope.name[char_num - 1]}를 선택 하셨습니다!");
-                        i++;
+                        if (char_num >= 22)
+                        {
+                            Console.WriteLine("그런 번호의 캐릭터는 존재 하지 않아요..");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{hope.name[char_num - 1]}를 선택 하셨습니다!");
+                            checkPlayer = char_num;
+                            i++;
+                        }
+
                     }
-                }  
+                } 
             }
         }
     }
 }
-
